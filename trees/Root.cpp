3517c18f1281178b5f168816ed77eb84b85a10ba
jcc333 <clemer19@gmail.com>
2015-07-02 14:29:40 -0500

    Delete empty cpp files for the moment

--- trees/Root.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by James Clemer on 5/17/15.
-// Copyright (c) 2015 clem. All rights reserved.
-//
-
-#include "Root.h"
-#include <set>
--- trees/VertexLRT.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by James Clemer on 5/23/15.
-// Copyright (c) 2015 clem. All rights reserved.
-//
-
-#include "VertexLRT.h"
-
